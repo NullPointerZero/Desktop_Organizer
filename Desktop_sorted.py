915c100edce8e47266ebfbc67a68edf427b9fa94
NullPointerZero <Mar-Hoff@mailbox.org>
2024-02-18 21:58:52 +0100

    did forget to add the new File:D

--- Desktop_sorted.py
+++ Desktop_sorted.py
@@ -6,8 +6,11 @@
 
 # all the Resources that are needed:
 import json                                         # sice a json file is used, the module is needed
-import os                                           # is needed to manipulate files
+import os                                           # is needed to manipulate Files
+from Desktop_Organizer_Functions import *           # Usually it is bad practice to import everything, but this file is made to fit, so we need everything.
 
+# Some Initial Values go Here
+current_path = os.getcwd()                          # The current Path may be needed as a parent folder
 
 # Initialise the Program by loading the json file and putting it into a dictionary. 
 # This way we can later check wether the key aka.fileending is in the dictionary and use the value as filepath 
@@ -20,10 +23,21 @@ f = open("whitelist.txt","r")
 whitelist = set()                                   # Here the set is built. A set is fast and doesnt allow duplicates.
 while True:
     line = f.readline()
-    whitelist.add(line.replace("\n","")             # Every Line ends with \n. This has to be cut, otherwise the comparison with that string will fail
+    whitelist.add(line.replace("\n",""))             # Every Line ends with \n. This has to be cut, otherwise the comparison with that string will fail
     if not line:                                    # If the line is empy, that means that the file is read. Now the loop has to be ended.
         break
 f.close()
 
 
+# This is just for testing purpose. I ll need it for my next coding session to remember what i did and what to do. Delete later.
+foldercontent = os.listdir()
+make_directories(foldercontent, current_path, whitelist)
 
+"""
+This is the mechanic to move the files. I have to come up with a mechanic to check wether a file with the same name already exists, since it will be overwritten otherwise.
+
+current_path_and_name = os.path.join(current_path, foldercontent[3])
+destination_path_and_name = os.path.join(current_path,"sorted",dir_name,foldercontent[3])
+os.rename(current_path_and_name, destination_path_and_name)
+
+"""
